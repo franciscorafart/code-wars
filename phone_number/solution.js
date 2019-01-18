13c82f685f68f71214092facc0edaa2bbbbf740e
Francisco Rafart <francisco.rafart@gmail.com>
2019-01-18 07:59:37 -0500

    Solution withjoin

--- phone_number/solution.js
+++ phone_number/solution.js
@@ -1,19 +1,10 @@
 function createPhoneNumber(numbers){
 
-let a = give_string(numbers.splice(0,3))
-let b = give_string(numbers.splice(0,3))
-let c = give_string(numbers.splice(0,4))
+let a = numbers.splice(0,3).join('')
+let b = numbers.splice(0,3).join('')
+let c = numbers.splice(0,4).join('')
 
 let result = '('+a+') '+b+'-'+c
 
 return result
-}
-
-give_string = st => {
-  let a = ''
-  for (let s of st) {
-    let b = a.concat(s)
-    a = b
-  }
-  return a
 }
