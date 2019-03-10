6b06a41e42fff0f560a87b96bb27939cedb591a6
Francisco Rafart <francisco.rafart@gmail.com>
2019-03-10 15:36:38 -0400

    Correction to solution when minutes > 0

--- clock-in-mirror/clock.js
+++ clock-in-mirror/clock.js
@@ -1,36 +1,29 @@
-const transform = (hoursMirr, minutesMirr) => {
-	// to int
-	let hours = Number(hoursMirr)
-	let minutes = Number(minutesMirr)
+const WhatIsTheTime = timeInMirror => {
+	const mirrorArray = timeInMirror.split(":");
+    let hours = Number(mirrorArray[0])
+	let minutes = Number(mirrorArray[1])
 
-//transform mirror
-  if (minutes > 0){
-    switch(hours){
-      //edge cases
-      case 12: hours = '11'; break;
-      case 11: hours = '12'; break
-      case 6: hours = '5'; break;
-      case 5: hours = '6'; break;
-      default: hours = String(Math.abs(12 - hours)); break;
+    //transform mirror
+    if (minutes > 0){
+        switch(hours){
+          //edge cases
+          case 12: hours = '11'; break;
+          case 11: hours = '12'; break
+          case 6: hours = '5'; break;
+          case 5: hours = '6'; break;
+          default: hours = String(Math.abs(11 - hours)); break;
+        }
+    } else {
+        hours = (hours == 12)? '12': String(Math.abs(12 - hours));
     }
-  } else {
-    hours = (hours == 12)? '12': String(Math.abs(12 - hours));
-  }
 
 	minutes != 0? minutes = String(Math.abs(60-minutes)): minutes = '0';
 
 	//To string
 	if (hours.length===1)
-    hours = "0"+hours
+        hours = "0"+hours
     if (minutes.length === 1)
-    minutes = "0"+minutes
+        minutes = "0"+minutes
 
-    return [hours, minutes]
-}
-
-const WhatIsTheTime = timeInMirror => {
-	const mirrorArray = timeInMirror.split(":");
-	const time = transform(mirrorArray[0], mirrorArray[1]);
-
-	return time[0]+":"+time[1]
+	return  hours +":"+minutes
 }
