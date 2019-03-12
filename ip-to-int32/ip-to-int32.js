c96bcc8d35be87ce9cef1aac0954cd87d507b423
Francisco Rafart <francisco.rafart@gmail.com>
2019-03-12 07:11:33 -0400

    Consider case 0

--- ip-to-int32/ip-to-int32.js
+++ ip-to-int32/ip-to-int32.js
@@ -4,27 +4,23 @@ function ipToInt32(ip){
 	return thirtyTwoBit(binaryNums)
 }
 
-// if reminder of % == 0 then stop
-// if reminder% < reminder - add value and continue
-// if reminder == reminder don’t do anything
-
 const getBinary = num => {
 	let reminder = num;
 	let binary = "00000000"
 
-  for(let i=7;i>=0;i--){
-		let index = Math.abs(i-7)
-		const new_reminder = reminder%Math.pow(2,i)
+	if (num > 0){
+    for(let i=7;i>=0;i--){
+  		let index = Math.abs(i-7)
+  		const new_reminder = reminder%Math.pow(2,i)
 
-    if(new_reminder==0){
-  	  binary = stringSplice(binary,index, 1,"1"); //replace value at index with one
-  	  break;
-    } else if (new_reminder<reminder) {
-  	  binary = stringSplice(binary,index,1,"1");
-  	  reminder = new_reminder
+      if (new_reminder==0 || new_reminder<reminder){
+        binary = stringSplice(binary,index, 1,"1"); //replace value at index with one
+        if (new_reminder==0)
+          break;
+        reminder = new_reminder
+      }
     }
   }
-
   return binary
 }
 
@@ -40,7 +36,6 @@ const thirtyTwoBit = binaryArray => {
   	  result += Math.pow(2, i)
     }
   }
-
   return result
 }
 
